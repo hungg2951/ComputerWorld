8e50bf8ca94f2abf0f2f805f0260df8f2f7f38e0
hung2951 <hungtv2951@gmail.com>
2025-03-16 15:03:36 +0700

    update render product by type_id in home page

--- src/Client/Pages/home/products/index.jsx
+++ src/Client/Pages/home/products/index.jsx
@@ -24,106 +24,119 @@ const Products = () => {
         console.log(e);
       });
   }, []);
+
+  // Nhóm sản phẩm theo laptop types
+  const groupedProducts = dataProductDetails.reduce((i, product) => {
+    if (!i[product.product_id.type_id._id]) {
+      i[product.product_id.type_id._id] = [];
+    }
+    i[product.product_id.type_id._id].push(product);
+    return i;
+  }, {});
   return (
     <>
       <div className="container mx-auto mt-10">
-        {/* Title + Price Filter */}
-        <div className="relative flex flex-col md:flex-row items-center max-sm:items-start bg-gray-100 max-sm:bg-white mb-5">
-          {/* Title chéo góc */}
-          <div className="bg-[#3c80ba] text-white font-bold py-2 text-base md:text-lg relative inline-block">
-            <div className="absolute top-0 right-0 w-6 md:w-7 h-full bg-white transform skew-x-[-30deg] origin-bottom-right"></div>
-            <h2 className="font-bold text-base md:text-lg px-6 md:px-2 md:pr-6">
-              LAPTOP GAMING
-            </h2>
-          </div>
-          <button className="md:hidden max-sm:block flex items-center gap-2 bg-blue-500 text-white px-4 py-2 text-sm font-semibold rounded hover:bg-blue-600 transition">
-            Xem tất cả <FaAngleDoubleRight />
-          </button>
-          {/* Price Filters */}
-          <div className="text-xs max-sm:hidden md:text-sm  text-gray-700 pl-4 md:pl-7 flex flex-wrap gap-2 md:gap-3 justify-center md:justify-start mt-2 md:mt-0">
-            <span className="font-semibold">Mức giá:</span>
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              5 TRIỆU - 10 TRIỆU
-            </span>{" "}
-            |
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              10 TRIỆU - 20 TRIỆU
-            </span>{" "}
-            |
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              20 TRIỆU - 30 TRIỆU
-            </span>{" "}
-            |
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              30 TRIỆU - 40 TRIỆU
-            </span>{" "}
-            |
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              40 TRIỆU - 50 TRIỆU
-            </span>{" "}
-            |
-            <span className="text-blue-600 cursor-pointer hover:underline">
-              TRÊN 50 TRIỆU
-            </span>
-          </div>
-        </div>
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          slidesPerView={1}
-          spaceBetween={15}
-          pagination={false}
-          autoplay={{ delay: 3000 }}
-          breakpoints={{
-            640: { slidesPerView: 1, spaceBetween: 15 },
-            768: { slidesPerView: 2, spaceBetween: 20 },
-            1024: { slidesPerView: 4, spaceBetween: 25 },
-          }}
-          className="w-full"
-        >
-          {dataProductDetails &&
-            dataProductDetails.map((product) => (
-              <SwiperSlide key={product.id}>
-                <NavLink to={`/product/${product.slug}`}>
-                  <div className="relative border p-4 rounded-lg shadow-md bg-white h-80">
-                    {product.discount && (
-                      <span className="absolute top-2 left-2 bg-red-500 text-white px-2 py-1 text-xs md:text-sm font-semibold rounded-full">
-                        {product.discount}
-                      </span>
-                    )}
-                    <img
-                      src={product.product_id.image}
-                      className="w-full h-40 md:h-48 lg:h-52 object-cover rounded-md"
-                    />
-                    <h3
-                      className="text-center mt-2 text-[13px] text-[#333] overflow-hidden"
-                      style={{
-                        display: "-webkit-box",
-                        WebkitLineClamp: 2,
-                        WebkitBoxOrient: "vertical",
-                      }}
-                    >
-                      <span className="capitalize">
-                        {`[${product.status}]`}{" "}
-                      </span>
-                      <span>{product.product_id.name} </span>
-                      <span>{product.year} </span>
-                      <span>{product.name}</span>
-                    </h3>
-                    <div className="flex items-center justify-center gap-2">
-                      <p className="text-center text-red-600 font-medium text-base md:text-lg">
-                        {formatCurrency(product.price)}
-                      </p>
-                      {product.oldPrice && (
-                        <p className="text-center text-gray-500 line-through text-sm md:text-base">
-                          {product.oldPrice}
-                        </p>
-                      )}
-                    </div>
-                  </div>
-                </NavLink>
-              </SwiperSlide>
-            ))}
-        </Swiper>
+        {groupedProducts &&
+          Object.entries(groupedProducts).map(([type_id, productList]) => (
+            <div className="mb-10">
+              <div className="relative flex flex-col md:flex-row items-center max-sm:items-start bg-gray-100 max-sm:bg-white mb-5">
+                {/* Title chéo góc */}
+                <div className="bg-[#3c80ba] text-white font-bold py-2 text-base md:text-lg relative inline-block">
+                  <div className="absolute top-0 right-0 w-6 md:w-7 h-full bg-white transform skew-x-[-30deg] origin-bottom-right"></div>
+                  <h2 className="font-bold text-base md:text-lg px-6 md:px-2 md:pr-6">
+                    {productList[0].product_id.type_id.name}
+                  </h2>
+                </div>
+                <button className="md:hidden max-sm:block flex items-center gap-2 bg-blue-500 text-white px-4 py-2 text-sm font-semibold rounded hover:bg-blue-600 transition">
+                  Xem tất cả <FaAngleDoubleRight />
+                </button>
+                {/* Price Filters */}
+                <div className="text-xs max-sm:hidden md:text-sm  text-gray-700 pl-4 md:pl-7 flex flex-wrap gap-2 md:gap-3 justify-center md:justify-start mt-2 md:mt-0">
+                  <span className="font-semibold">Mức giá:</span>
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    5 TRIỆU - 10 TRIỆU
+                  </span>{" "}
+                  |
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    10 TRIỆU - 20 TRIỆU
+                  </span>{" "}
+                  |
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    20 TRIỆU - 30 TRIỆU
+                  </span>{" "}
+                  |
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    30 TRIỆU - 40 TRIỆU
+                  </span>{" "}
+                  |
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    40 TRIỆU - 50 TRIỆU
+                  </span>{" "}
+                  |
+                  <span className="text-blue-600 cursor-pointer hover:underline">
+                    TRÊN 50 TRIỆU
+                  </span>
+                </div>
+              </div>
+              <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                slidesPerView={1}
+                spaceBetween={15}
+                pagination={false}
+                autoplay={{ delay: 3000 }}
+                breakpoints={{
+                  640: { slidesPerView: 1, spaceBetween: 15 },
+                  768: { slidesPerView: 2, spaceBetween: 20 },
+                  1024: { slidesPerView: 4, spaceBetween: 25 },
+                }}
+                className="w-full"
+              >
+                {productList &&
+                  productList.map((product) => (
+                    <SwiperSlide key={product.id}>
+                      <NavLink to={`/product/${product.slug}`}>
+                        <div className="relative border p-4 rounded-lg shadow-md bg-white h-80">
+                          {product.discount && (
+                            <span className="absolute top-2 left-2 bg-red-500 text-white px-2 py-1 text-xs md:text-sm font-semibold rounded-full">
+                              {product.discount}
+                            </span>
+                          )}
+                          <img
+                            src={product.product_id.image}
+                            className="w-full h-40 md:h-48 lg:h-52 object-cover rounded-md"
+                          />
+                          <h3
+                            className="text-center mt-2 text-[13px] text-[#333] overflow-hidden"
+                            style={{
+                              display: "-webkit-box",
+                              WebkitLineClamp: 2,
+                              WebkitBoxOrient: "vertical",
+                            }}
+                          >
+                            <span className="capitalize">
+                              {`[${product.status}]`}{" "}
+                            </span>
+                            <span>{product.product_id.name} </span>
+                            <span>{product.year} </span>
+                            <span>{product.name}</span>
+                          </h3>
+                          <div className="flex items-center justify-center gap-2">
+                            <p className="text-center text-red-600 font-medium text-base md:text-lg">
+                              {formatCurrency(product.price)}
+                            </p>
+                            {product.oldPrice && (
+                              <p className="text-center text-gray-500 line-through text-sm md:text-base">
+                                {product.oldPrice}
+                              </p>
+                            )}
+                          </div>
+                        </div>
+                      </NavLink>
+                    </SwiperSlide>
+                  ))}
+              </Swiper>
+            </div>
+          ))}
       </div>
     </>
   );
